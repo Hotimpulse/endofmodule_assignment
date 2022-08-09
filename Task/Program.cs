1b66b7daf21b9fb2a005445e5f5c83b0fe2e924d
Hotimpulse <jz2@rambler.ru>
2022-08-09 17:35:28 +0300

    add csharp code file

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] originalArray = {"hello", "2", "world", ":-)"};
+
+Console.WriteLine("Example array is: [" + string.Join(", ", originalArray) + "]");
+
+Console.Write("Array of strings that are < 4 symbols in length: ");
+
+int count = 0;
+for (int i = 0; i < originalArray.Length; i++)
+{
+    if (originalArray[i].Length < 4) {
+        count++;
+    }
+}
+
+string[] sortedArray = new string[count];
+int result = 0;
+
+for (int i = 0; i < 4; i++)
+{
+    if (originalArray[i].Length < 4) {
+        sortedArray[result++] = originalArray[i];
+    }
+}
+
+Console.Write($"[{String.Join(", ", sortedArray)}]");
